43765ac040dea3fdf578833215a1b5e9828e7fe7
kjuraszek <donkrissocorleone@gmail.com>
2021-12-03 20:31:44 +0100

    Day 3

---
+++ day_3/day_3.py
@@ -0,0 +1,81 @@
+'''
+Advent of Code - Day 3
+'''
+
+import os
+import pathlib
+import collections
+
+
+_DIAGNOSTIC_FILE = 'day_3.txt'
+
+class DiagnosticTool():
+    def __init__(self, data_file) -> None:
+        self.diagnostic_data = []
+        self.diagnostic_data_width = 0
+        self.most_common_bits = []
+        self.least_common_bits = []
+        self.load_data(data_file)
+        self.get_data_width()
+        self.get_common_bits()
+        
+    def load_data(self, data_file):
+        self.diagnostic_data = [line.strip() for line in open(os.path.join(pathlib.Path(__file__).parent,  # pylint: disable=R1732,W1514 # noqa: E501
+                                                           data_file), 'r')]
+
+    def get_data_width(self):
+        self.diagnostic_data_width = len(self.diagnostic_data[0])
+        
+    def get_common_values(self, diagnostic_data, index):
+        return collections.Counter([row[index] for row in diagnostic_data]).most_common()
+
+    def get_common_bits(self):
+        for index in range(self.diagnostic_data_width):
+            most_common = self.get_common_values(self.diagnostic_data, index)
+            self.most_common_bits.append(most_common[0][0])
+            self.least_common_bits.append(most_common[1][0])
+        
+    def get_gamma_rate(self):
+        return int(''.join(self.most_common_bits), 2)
+
+    def get_epsilon_rate(self):
+        return int(''.join(self.least_common_bits), 2)
+
+    def get_oxygen_generator_rating(self):
+        return int(''.join(self.calculate_rating_by_bit(self.diagnostic_data, 0, self.most_common_bits[0], 'most')), 2)
+    
+    def get_co2_scrubber_rating(self):
+        return int(''.join(self.calculate_rating_by_bit(self.diagnostic_data, 0, self.least_common_bits[0], 'least')), 2)
+            
+    def calculate_rating_by_bit(self, data, position, bit, common):
+        if len(data) == 1:
+            return data[0]
+        else:
+            temp_data = []
+            most_common_bits = self.get_common_values(data, position)
+            if len(most_common_bits) == 1:
+                bit = most_common_bits[0][0]
+            else:
+                if common == 'most':
+                    if most_common_bits[0][1] == most_common_bits[1][1]:
+                        bit = '1'
+                    else:
+                        bit = most_common_bits[0][0]
+                else:
+                    if most_common_bits[0][1] == most_common_bits[1][1]:
+                        bit = '0'
+                    else:
+                        bit = most_common_bits[1][0]
+            #print(position, data)
+            for row in data:
+                if row[position] == bit:
+                    temp_data.append(row)
+            position += 1
+            
+            return self.calculate_rating_by_bit(temp_data, position, bit, common)
+
+
+diagnostic_tool = DiagnosticTool(_DIAGNOSTIC_FILE)
+
+print(f'Power consumption is: {diagnostic_tool.get_gamma_rate() * diagnostic_tool.get_epsilon_rate()}')
+print(f'Life support rating is: {diagnostic_tool.get_oxygen_generator_rating() * diagnostic_tool.get_co2_scrubber_rating()}')
